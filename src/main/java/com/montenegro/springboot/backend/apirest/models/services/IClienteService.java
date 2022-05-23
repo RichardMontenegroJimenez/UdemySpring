ba09150ab35139a0fde2e6ecb6ada90f837fea97
Richard <1903023@alu.murciaeduca.es>
2022-05-23 21:05:05 +0200

    first commit

---
+++ src/main/java/com/montenegro/springboot/backend/apirest/models/services/IClienteService.java
@@ -0,0 +1,9 @@
+package com.montenegro.springboot.backend.apirest.models.services;
+
+import java.util.List;
+
+import com.montenegro.springboot.backend.apirest.models.entity.Cliente;
+
+public interface IClienteService {
+	public List<Cliente> findAll();
+}
